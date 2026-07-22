1a615ce42f9786576467597afe18c6911e69ac0a
Mariano <marianosn123@gmail.com>
2026-07-22 16:36:25 -0600

    feat: mostrar MDisplayAOD sobre pantalla de bloqueo

--- app/src/main/java/com/NeroRonnin/mdisplayaod/MainActivity.kt
+++ app/src/main/java/com/NeroRonnin/mdisplayaod/MainActivity.kt
@@ -17,6 +17,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setShowWhenLocked(true)
+        setTurnScreenOn(true)
 
         MediaSessionHelper.syncCurrentSession(this)
 
